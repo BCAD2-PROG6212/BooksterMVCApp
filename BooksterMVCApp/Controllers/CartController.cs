8a014f475ac93657fcbffec4881e7ff2d3dfecf3
Ebrahim Adam <eb.adam@gmail.com>
2022-11-08 14:45:30 +0200

    Adding in checkout

--- BooksterMVCApp/Controllers/CartController.cs
+++ BooksterMVCApp/Controllers/CartController.cs
@@ -7,15 +7,71 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using BooksterMVCApp.Models;
 using BooksterMVCApp.Utils;
+using BooksterMVCApp.ViewModels;
 
 namespace BooksterMVCApp.Controllers
 {
     public class CartController : Controller
     {
+        private readonly BooksterContext _context;
+
+        public CartController(BooksterContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult ViewCart()
         {
             return View(CartUtil.cart);
         }
         
+        public IActionResult Checkout(string CurrentUser, decimal OrderTotal)
+        {
+            Customer c = _context.Customers.Where(cust => cust.CustomerEmail.Equals(CurrentUser)).First();
+            Address a = _context.Addresses.Where(addr => addr.CustomerId.Equals(c.CustomerId)).First();
+
+            CheckoutViewModel checkout = new CheckoutViewModel(c.CustomerId, c.CustomerName,
+                c.CustomerSurname, c.CustomerEmail, c.CustomerPhone, a.AddressId,
+                a.AddressLineOne, a.AddressLineTwo, a.City, a.PostCode, CartUtil.cart);
+
+            return View(checkout);
+
+        }
+
+        public IActionResult ConfirmOrder(string CurrentUser, decimal OrderTotal)
+        {
+            Customer c = _context.Customers.Where(cust => cust.CustomerEmail.Equals(CurrentUser)).First();
+            Address a = _context.Addresses.Where(addr => addr.CustomerId.Equals(c.CustomerId)).First();
+
+            Order o = new Order();
+            o.CustomerId = c.CustomerId;
+            o.OrderTotal = OrderTotal;
+            o.OrderDate = DateTime.Now;
+            o.PaymentStatus = "Complete";
+
+            _context.Orders.Add(o);
+            _context.SaveChanges();
+
+            Order writtenOrder = _context.Orders.Where(ord => ord.CustomerId.Equals(c.CustomerId)).OrderBy(sort => sort.OrderId).Last();
+
+            Delivery del = new Delivery();
+            del.OrderId = writtenOrder.OrderId;
+            del.AddressId = a.AddressId;
+            del.DeliveryDate = DateTime.Today.AddDays(5);
+            _context.Deliveries.Add(del);
+
+            foreach (var cartItem in CartUtil.cart)
+            {
+                OrderBook ob = new OrderBook();
+                ob.OrderId = writtenOrder.OrderId;
+                ob.BookId = cartItem.bookId;
+                ob.ItemPrice = cartItem.Price;
+                ob.Quantity = cartItem.Qty;
+                _context.OrderBooks.Add(ob);
+            }
+
+            _context.SaveChanges();
+            return View();
+        }
     }
 }
---
+++ BooksterMVCApp/ViewModels/CheckoutViewModel.cs
@@ -0,0 +1,34 @@
+﻿namespace BooksterMVCApp.ViewModels
+{
+    public class CheckoutViewModel
+    {
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerSurname { get; set; }
+        public string CustomerEmail { get; set; }
+        public string CustomerPhone { get; set; }
+
+        public int AddressId { get; set; }
+        public string AddressLineOne { get; set; }
+        public string AddressLineTwo { get; set; }
+        public string City { get; set; }
+        public string PostCode { get; set; }
+
+        public List<CartViewModel> cart { get; set; }
+
+        public CheckoutViewModel(int customerId, string customerName, string customerSurname, string customerEmail, string customerPhone, int addressId, string addressLineOne, string addressLineTwo, string city, string postCode, List<CartViewModel> cart)
+        {
+            CustomerId = customerId;
+            CustomerName = customerName;
+            CustomerSurname = customerSurname;
+            CustomerEmail = customerEmail;
+            CustomerPhone = customerPhone;
+            AddressId = addressId;
+            AddressLineOne = addressLineOne;
+            AddressLineTwo = addressLineTwo;
+            City = city;
+            PostCode = postCode;
+            this.cart = cart;
+        }
+    }
+}
